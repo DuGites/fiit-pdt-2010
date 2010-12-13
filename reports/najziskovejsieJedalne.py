cab8320179f4778e9e2b218425fa1062dd9d5252
immer.michal <immer.michal@56d5b1b6-1c9e-444f-05aa-cf9f89dc51f8>
2010-12-13 12:12:20 +0000

    prvy nastrel reportov

---
+++ reports/najziskovejsieJedalne.py
@@ -0,0 +1,26 @@
+import MySQLdb
+import sys
+
+# vypise najziskovejsie jedalne za dany rok, default je 2008
+
+db = MySQLdb.connect(host='localhost', user='root', passwd='', db='dwh')
+
+if (len(sys.argv) > 1):
+    year = sys.argv[1]
+else:
+    year = '2008'
+    
+query = db.cursor()
+report_source = 'select count(*),f.name from dwh.food f \
+join dwh.foodsale fs on (f.id = fs.food_id) \
+join dwh.`date` d on (fs.date_id = d.id) \
+where d.year = %s \
+group by f.name order by count(*) desc' %year
+
+query.execute(report_source) 
+output = query.fetchone()
+print output 
+
+
+
+
